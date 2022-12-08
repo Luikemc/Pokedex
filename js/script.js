45afe2b2e1ceb11394625c12e83e84663b28c7c6
Luiz Henrique Martins Chaves <102527990+Luikemc@users.noreply.github.com>
2022-12-08 14:58:21 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -57,3 +57,12 @@ buttonNext.addEventListener('click', () => {
 });
 
 renderPokemon(searchPokemon);
+
+function openNav(){
+  document.getElementById("myNav").style.width = '100%';
+ 
+}
+
+function closeNav(){
+  document.getElementById("myNav").style.width = '0%';
+}
